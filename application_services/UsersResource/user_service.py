ef384f554283cb84c387d38a97d44adeec6cb81d
EC2 Default User <ec2-user@ip-172-31-21-253.us-east-2.compute.internal>
2021-10-13 17:29:51 +0000

    changed database name and table name to match ec2 settings'

--- application_services/UsersResource/user_service.py
+++ application_services/UsersResource/user_service.py
@@ -18,6 +18,6 @@ class UserResource(BaseRDBApplicationResource):
 
     @classmethod
     def get_by_template(cls, template):
-        res = RDBService.find_by_template("CLOUD_USERS_INFO", "users", template,"everything")
+        res = RDBService.find_by_template("hw1_db", "user", template,"everything")
         print("THIS IS RES",res)
         return res
